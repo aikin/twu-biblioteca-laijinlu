21114c742dfaf7b0f6e26d49f499a6608d7706c0
aikin <me@ulaijn.com>
2015-09-25 15:25:50 +0800

    aikin: add BookRepo to provide list of books

--- src/main/java/com/twu/biblioteca/domain/repo/BookRepo.java
+++ src/main/java/com/twu/biblioteca/domain/repo/BookRepo.java
@@ -1,6 +1,37 @@
 package com.twu.biblioteca.domain.repo;
 
 
+import com.twu.biblioteca.domain.model.Book;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BookRepo {
 
+    private final Map<Integer, Book> listOfBooks = new HashMap<>();
+
+    public BookRepo() {
+        this.initBooks();
+    }
+
+    public Map<Integer, Book> getListOfBooks() {
+        return listOfBooks;
+    }
+
+
+    private void initBooks() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            listOfBooks.put(1, new Book(1, "Refactoring", "Martin Fowler & Kent Beck", formatter.parse("1999-07-08")));
+            listOfBooks.put(2, new Book(2, "Clean Code", "Robert C. Martin", formatter.parse("2008-08-11")));
+            listOfBooks.put(3, new Book(3, "Test Driven Development: By Example", "Kent Beck", formatter.parse("2002-11-18")));
+            listOfBooks.put(4, new Book(4, "The Art of Readable Code", "Dustin Boswell & Trevor Foucher", formatter.parse("2011-12-02")));
+            listOfBooks.put(5, new Book(5, "Patterns of Enterprise Application Architecture", "Martin Fowler", formatter.parse("2002-11-15")));
+            listOfBooks.put(6, new Book(6, "Extreme Programming Explained: Embrace Chang", "Kent Beck & Cynthia Andres ", formatter.parse("2011-09-01")));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/test/java/com/twu/biblioteca/domain/repo/BookRepoTest.java
@@ -0,0 +1,48 @@
+package com.twu.biblioteca.domain.repo;
+
+
+import com.twu.biblioteca.domain.model.Book;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BookRepoTest {
+
+    private BookRepo bookRepo;
+
+    private SimpleDateFormat formatter;
+
+    @Before
+    public void setUp() {
+        bookRepo = new BookRepo();
+        formatter = new SimpleDateFormat("yyyy-MM-dd");
+    }
+
+    @After
+    public  void tearDown() {
+        bookRepo = null;
+        formatter = null;
+    }
+
+    @Test
+    public void should_can_get_list_of_books() throws ParseException {
+        Map<Integer, Book> listOfBooks = bookRepo.getListOfBooks();
+
+        assertThat(listOfBooks.size(), is(6));
+        assertThat(listOfBooks.get(1).getId(), is(1));
+        assertThat(listOfBooks.get(1).getTitle(), is("Refactoring"));
+        assertThat(listOfBooks.get(1).getAuthor(), is("Martin Fowler & Kent Beck"));
+        assertThat(listOfBooks.get(1).getPublishedYear(), is(formatter.parse("1999-07-08")));
+        assertThat(listOfBooks.get(4).getId(), is(4));
+        assertThat(listOfBooks.get(4).getTitle(), is("The Art of Readable Code"));
+        assertThat(listOfBooks.get(4).getAuthor(), is("Dustin Boswell & Trevor Foucher"));
+        assertThat(listOfBooks.get(4).getPublishedYear(), is(formatter.parse("2011-12-02")));
+    }
+}
